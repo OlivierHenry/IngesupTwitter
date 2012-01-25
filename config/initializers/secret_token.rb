14c335972ac0381d9b71ebd52fda9c1fb30fcc35
Olivier HENRY <olivier.henry@ingesup.com>
2012-01-25 14:30:41 +0100

    creation du projet

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IngesupTwitter::Application.config.secret_token = 'dc3b098bb2029032b42247b4ce39eba91bb1df0310689da8a2e36f4fd3c31558f0bf5df6947351a43a5c797e5fc5484d56e961f33a4083bc1529b221dba179b4'
